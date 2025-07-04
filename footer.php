1414a971905b93bfd2748d5119edd51dc5467cff
rajiharraki <rajih.a93@gmail.com>
2025-07-04 20:53:55 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+</main>
+<footer style="padding:32px 0 16px 0;text-align:center;color:#999;font-size:0.9em;background:#f3f3fa;">
+    <div style="max-width:1100px;margin:auto;">
+        &copy; <?php echo date('Y'); ?> Arrastay. All rights reserved.
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
